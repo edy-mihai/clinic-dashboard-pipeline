de0fb4b5d429877d7351355c7b4bcc026ad58b79
edy-mihai <edypetco@gmail.com>
2026-08-18 20:15:58 +0300

    test df

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-INPUT_DATA_PATH = "data/dummy database.xlsx"
+INPUT_DATA_PATH = "data/input/dummy_database.xlsx"
 SHEET_NAME = "Foaie 1 (2)"
 OUTPUT_DIR = "data/output/"
 EURO_RATE = 4.97
---
+++ src/extract.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from config import INPUT_DATA_PATH, SHEET_NAME
+
+def load_clinic_data():
+    df = pd.read_excel(INPUT_DATA_PATH, sheet_name=SHEET_NAME)
+    return df
+
+test_df = load_clinic_data()
+print(test_df.head())
